89d48f69d05c98a5783bb675ef2b38020e75b53f
EnkhAmar <enkhamar1220@gmail.com>
2021-03-13 18:35:48 +0800

    overflow-x: hidden

--- script.js
+++ script.js
@@ -104,19 +104,7 @@ function generateEachMonth (indexOfThatMonth) {
                                 <div>Sat</div>
                             </div>
                             <div class="calendar-days" id="${month_id[indexOfThatMonth]}"></div>
-                        </div>
-                        <div class="calendar-body">
-                        <div class="calendar-week-day">
-                            <div>Sun</div>
-                            <div>Mon</div>
-                            <div>Tue</div>
-                            <div>Wed</div>
-                            <div>Thu</div>
-                            <div>Fri</div>
-                            <div>Sat</div>
-                        </div>
-                        <div class="calendar-days" id="${month_id[indexOfThatMonth]}"></div>
-                    </div>`;
+                        </div>`;
 
     container.appendChild(newMonth);
     generateCalendar(indexOfThatMonth, curr_year.value);
